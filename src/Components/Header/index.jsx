5ba3cc85d2b7160592e0b209eb6d1a0da93a19f6
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-03-25 15:56:43 -0300

    Finished header HTML and CSS

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './index.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../../Assets/dogs.svg";
+import styles from "./index.module.css";
 
 const Header = () => (
-  <nav className={`container ${styles.nav}`}>
-    <Link to="/">Home</Link>
-    <Link to="/login">Login / Criar</Link>
-  </nav>
+  <header className={styles.header}>
+    <nav className={`container ${styles.nav}`}>
+      <Link to="/" className={styles.logo} arial-label="Dogs - Home">
+        <Dogs />
+      </Link>
+      <Link to="/login" className={styles.login}>
+        Login / Criar
+      </Link>
+    </nav>
+  </header>
 );
 
 export default Header;
