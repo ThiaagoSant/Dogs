c08aec48d04154523f0a69fea93dd7a7d9f93c9b
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-03-30 14:35:47 -0300

    Initial Login routes

---
+++ src/Components/Login/LoginForm/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LoginForm = () => (
+  <div>
+    Login Form
+    <Link to='/login/criar'>Cadastros</Link>
+  </div>
+);
+
+export default LoginForm;
---
+++ src/Components/Login/LoginPasswordReset/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPasswordReset = () => (
+  <div>LoginPasswordReset</div>
+);
+
+export default LoginPasswordReset;
