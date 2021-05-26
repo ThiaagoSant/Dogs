d20a7d3c3414237272aeeedc1dc4a205b35b8df3
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-05-26 01:23:45 -0300

    Feat: Created Forms components

--- src/Components/Login/LoginForm/index.jsx
+++ src/Components/Login/LoginForm/index.jsx
@@ -1,48 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Input from '../../Forms/Input';
+import Button from '../../Forms/Button';
+
 const LoginForm = () => {
-	const [username, setUsername] = React.useState(''),
-		[password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState(''),
+    [password, setPassword] = React.useState('');
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		const fetchProps = {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ username, password }),
-			},
-			resp = await fetch(
-				'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
-				fetchProps
-			),
-			json = await resp.json();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const fetchProps = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      },
+      resp = await fetch(
+        'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
+        fetchProps
+      ),
+      json = await resp.json();
 
-		return json;
-	};
+    return json;
+  };
 
-	return (
-		<section>
-			<h1>Login Form</h1>
-			<form action='#' onSubmit={handleSubmit}>
-				<input
-					value={username}
-					type='text'
-					onChange={({ target }) => setUsername(target.value)}
-				/>
-
-				<input
-					value={password}
-					type='text'
-					onChange={({ target }) => setPassword(target.value)}
-				/>
-				<button>Entrar</button>
-			</form>
-			<Link to='/login/criar'>Cadastros</Link>
-		</section>
-	);
+  return (
+    <section>
+      <h1>Login Form</h1>
+      <form action='#' onSubmit={handleSubmit}>
+        <Input label='Usuário' type='text' name='username' />
+        <Input label='Senha' type='password' name='password' />
+        <Button>Entrar</Button>
+      </form>
+      <Link to='/login/criar'>Cadastros</Link>
+    </section>
+  );
 };
 
 export default LoginForm;
